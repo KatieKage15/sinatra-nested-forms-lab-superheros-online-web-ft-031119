74a0359106e2459d64bf7c5b15f300b6857ae0f2
victorious-messaging-4309 <wildayk@gmail.com>
2019-05-01 15:09:52 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -2,9 +2,15 @@ class SuperHero
   attr_accessor :name, :power, :bio
 
   @@all = []
-  
+
   def initialize(:name, :power, :bio)
-    @name = name
-    @power = power
-    @bio = bio
+    @name = params[:name]
+    @power = params[:power]
+    @bio = params[:bio]
+    self.save
   end
+
+  def save
+    self.class.all << self
+  end
+end
